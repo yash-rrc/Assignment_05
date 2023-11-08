9dff85ef1310a6e8cf125ce595d785e107b69c11
yash <ychaudhari@rrc.ca>
2023-11-08 14:42:56 -0600

    Updated readme file

--- src/chatbot.py
+++ src/chatbot.py
@@ -2,8 +2,8 @@
 Description: Chatbot application.  Allows user to perform 
 balance inquiries and make deposits to their accounts.
 Author: ACE Department
-Modified by: {Student Name}
-Date: 2023-10-15
+Modified by: {Yash}
+Date: 2023-11-08
 Usage: From the console: python src/chatbot.py
 """
 
